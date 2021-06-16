951cda7cfa21795044ee81aca1e8af420fa77652
omeriku <73793617+omeriku@users.noreply.github.com>
2021-06-16 14:42:11 +0300

    logout

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
             User
           </template>
           <b-dropdown-item href="#">Favorites</b-dropdown-item>
-          <b-dropdown-item href="#">Log Out</b-dropdown-item>
+          <b-dropdown-item href="#" @click="Logout()">Log Out</b-dropdown-item>
         </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
@@ -33,7 +33,9 @@ export default {
     Logout() {
       this.$root.store.logout();
       this.$root.toast("Logout", "User logged out successfully", "success");
-
+      const response = this.axios.post(
+          "http://localhost:3000/logout",
+        );
       this.$router.push("/").catch(() => {
         this.$forceUpdate();
       });
